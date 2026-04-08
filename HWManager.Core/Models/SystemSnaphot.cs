320c5597953d67a549a30e804b679bb692a364bf
HONG <hkt03130@gmail.com>
2026-04-08 18:38:27 +0900

    하드웨어 수집 코드 분리를 위한 클래스

---
+++ HWManager.Core/Models/SystemSnaphot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWManager.Core.Models
+{
+    internal class SystemSnaphot
+    {
+    }
+}
