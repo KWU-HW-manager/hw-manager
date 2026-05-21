f57e257a424201005976c931c0120cd9f1802904
HONG <hkt03130@gmail.com>
2026-05-22 00:08:47 +0900

    ConFigForm 생성

--- src/HWManager.Client/ConfigForm.Designer.cs
+++ src/HWManager.Client/ConfigForm.Designer.cs
@@ -17,86 +17,89 @@ namespace HWManager.Client
 
         private void InitializeComponent()
         {
-            tabControl = new TabControl();
+            panelContent = new Panel();
             tableLayoutPanelButtons = new TableLayoutPanel();
-            btnOK = new Button();
+            btnSave = new Button();
             btnCancel = new Button();
 
-            tabControl.SuspendLayout();
             tableLayoutPanelButtons.SuspendLayout();
             SuspendLayout();
 
-            // tabControl
-            tabControl.Dock = DockStyle.Fill;
-            tabControl.Location = new Point(0, 0);
-            tabControl.Name = "tabControl";
-            tabControl.SelectedIndex = 0;
-            tabControl.Size = new Size(500, 400);
-            tabControl.TabIndex = 0;
+            // panelContent - ¼³Á¤ ³»¿ëÀÌ µé¾î°¥ ¿µ¿ª
+            panelContent.BackColor = Color.White;
+            panelContent.Dock = DockStyle.Fill;
+            panelContent.Location = new Point(0, 0);
+            panelContent.Name = "panelContent";
+            panelContent.Padding = new Padding(15);
+            panelContent.Size = new Size(600, 340);
+            panelContent.TabIndex = 0;
 
-            // tableLayoutPanelButtons
+            // tableLayoutPanelButtons - ¹öÆ° ·¹ÀÌ¾Æ¿ô
+            tableLayoutPanelButtons.BackColor = Color.WhiteSmoke;
             tableLayoutPanelButtons.ColumnCount = 2;
             tableLayoutPanelButtons.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanelButtons.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanelButtons.Controls.Add(btnOK, 0, 0);
+            tableLayoutPanelButtons.Controls.Add(btnSave, 0, 0);
             tableLayoutPanelButtons.Controls.Add(btnCancel, 1, 0);
             tableLayoutPanelButtons.Dock = DockStyle.Bottom;
-            tableLayoutPanelButtons.Location = new Point(0, 360);
+            tableLayoutPanelButtons.Location = new Point(0, 340);
             tableLayoutPanelButtons.Name = "tableLayoutPanelButtons";
+            tableLayoutPanelButtons.Padding = new Padding(10);
             tableLayoutPanelButtons.RowCount = 1;
             tableLayoutPanelButtons.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
-            tableLayoutPanelButtons.Size = new Size(500, 40);
+            tableLayoutPanelButtons.Size = new Size(600, 60);
             tableLayoutPanelButtons.TabIndex = 1;
 
-            // btnOK
-            btnOK.Dock = DockStyle.Fill;
-            btnOK.Font = new Font("¸¼Àº °íµñ", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            btnOK.Location = new Point(5, 5);
-            btnOK.Margin = new Padding(5);
-            btnOK.Name = "btnOK";
-            btnOK.Size = new Size(240, 30);
-            btnOK.TabIndex = 0;
-            btnOK.Text = "È®ÀÎ";
-            btnOK.UseVisualStyleBackColor = true;
-            btnOK.Click += btnOK_Click;
+            // btnSave
+            btnSave.BackColor = Color.FromArgb(79, 129, 189);
+            btnSave.Dock = DockStyle.Fill;
+            btnSave.Font = new Font("¸¼Àº °íµñ", 9F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnSave.ForeColor = Color.White;
+            btnSave.Margin = new Padding(5);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(285, 40);
+            btnSave.TabIndex = 0;
+            btnSave.Text = "ÀúÀå";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
 
             // btnCancel
+            btnCancel.BackColor = Color.LightGray;
             btnCancel.Dock = DockStyle.Fill;
-            btnCancel.Font = new Font("¸¼Àº °íµñ", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            btnCancel.Location = new Point(255, 5);
+            btnCancel.Font = new Font("¸¼Àº °íµñ", 9F, FontStyle.Regular, GraphicsUnit.Point, 129);
             btnCancel.Margin = new Padding(5);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(240, 30);
+            btnCancel.Size = new Size(285, 40);
             btnCancel.TabIndex = 1;
             btnCancel.Text = "Ãë¼Ò";
             btnCancel.UseVisualStyleBackColor = true;
             btnCancel.Click += btnCancel_Click;
 
             // ConfigForm
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(500, 400);
-            Controls.Add(tabControl);
+            ClientSize = new Size(600, 400);
+            Controls.Add(panelContent);
             Controls.Add(tableLayoutPanelButtons);
-            Font = new Font("¸¼Àº °íµñ", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            Font = new Font("¸¼Àº °íµñ", 9F, FontStyle.Regular, GraphicsUnit.Point, 129);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "ConfigForm";
             StartPosition = FormStartPosition.CenterParent;
             Text = "¼³Á¤";
+            Load += ConfigForm_Load;
 
-            tabControl.ResumeLayout(false);
             tableLayoutPanelButtons.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private TabControl tabControl;
-        private TableLayoutPanel tableLayoutPanelButtons;
-        private Button btnOK;
+        private Button btnSave;
         private Button btnCancel;
+        private Panel panelContent;
+        private TableLayoutPanel tableLayoutPanelButtons;
     }
 }
